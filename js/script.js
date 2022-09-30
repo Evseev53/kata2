c981d9a89d2e6ecd6873bbe4ddd127a6535fbb39
Sergey Evseev <yellow_ball@inbox.ru>
2022-09-30 23:01:57 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -1,21 +1,50 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
+let windowInnerWidth = window.innerWidth;
+let swiperClass = document.querySelector('.swiper-wrapper');
+let swiperSlideClass = document.querySelectorAll('.swiper-slide');
+console.log(swiperSlideClass);
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+if (windowInnerWidth < 768) {
+    let swiper = new Swiper('.swiper', {
+        // Optional parameters
+        loop: true,
 
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        slidesPerView: 1.2,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1.2,
+                spaceBetween: 20
+            },
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // when window width is >= 640px
+            640: {
+                slidesPerView: 2.7,
+                spaceBetween: 20
+            }
+        }
+
+    });
+}
+
+if (windowInnerWidth >= 768) {
+    swiperClass.classList.remove('swiper-wrapper');
+    for(let i = 0; i < swiperSlideClass.length; i++) {
+        swiperSlideClass[i].classList.remove('swiper-slide');
+    }
+}
+
+window.addEventListener("resize", function () {
+
 });
+
