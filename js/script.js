90cd03457a632df6b94dba01050fe65b5c19c0d4
Sergey Evseev <yellow_ball@inbox.ru>
2022-10-11 15:46:15 +0300

    assembly-sidebar

--- js/script.js
+++ js/script.js
@@ -1,30 +1,14 @@
 let cardsVisible = false; //все карточки отображаются
 let showMoreButton = document.querySelector('.read-more');
 let swiperActive = false; //свайпер отображается
-let swiper;
+let slider;
+let buttonBurger = document.querySelector('.nav-list__link--burger-pink'); //кнопка бургер
+let sidebar = document.querySelector('.sidebar');
+let sidebarClose = document.querySelector('.nav-list__link--back-pink'); //кнопка закрыть сайдбар
 
-//при загрузке страницы включает свайпер, если <768
-if (window.screen.width < 768) {
-    swiperActive = true;
-    swiper = new Swiper('.swiper', {
-        loop: false,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        slidesPerView: 'auto',
-        spaceBetween: 18,
-    });
-}
-
-//вкл и выкл свайпера при ресайзе
-window.addEventListener("resize", function () {
-    if (window.screen.width >= 768 && swiperActive) {
-        swiper.destroy();
-        swiperActive = false;
-        } else if (window.screen.width < 768 && !swiperActive) {
-        cardsVisible = false;
-        swiper = new Swiper('.swiper', {
+function swiper(on) {
+    if (on) {
+        slider = new Swiper('.swiper', {
             loop: false,
             pagination: {
                 el: '.swiper-pagination',
@@ -33,6 +17,25 @@ window.addEventListener("resize", function () {
             slidesPerView: 'auto',
             spaceBetween: 18,
         });
+    } else {
+        slider.destroy();
+    }
+}
+
+//при загрузке страницы включает свайпер, если <768
+if (window.screen.width < 768) {
+    swiperActive = true;
+    swiper(true);
+}
+
+//вкл и выкл свайпера при ресайзе
+window.addEventListener("resize", function () {
+    if (window.screen.width >= 768 && swiperActive) {
+        swiper(false);
+        swiperActive = false;
+        } else if (window.screen.width < 768 && !swiperActive) {
+        cardsVisible = false;
+        swiper(true);
         swiperActive = true;
     }
 });
@@ -52,4 +55,22 @@ showMoreButton.addEventListener('click', function () {
     }
 });
 
-//кнопка меню бургер
+
+//показать сайдбар
+buttonBurger.addEventListener('click', function () {
+    sidebar.style.display = 'flex';
+});
+
+//скрыть сайдбар
+sidebarClose.addEventListener('click', function () {
+    sidebar.style.display = 'none';
+});
+
+//сайдбар при ресайзе
+window.addEventListener('resize', function () {
+    if (window.screen.width >= 1120) {
+        sidebar.style.display = 'flex';
+    } else {
+        sidebar.style.display = 'none';
+    }
+});
